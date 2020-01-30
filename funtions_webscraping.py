64c2ad37d8bf5caeba8952fce254f423cae36cdb
josephdicastro <joe.dicastro@gmail.com>
2020-01-29 19:49:49 -0500

    TOC scraped completed

---
+++ funtions_webscraping.py
@@ -0,0 +1,70 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+import time
+import os
+
+##### Helper Functions for Web Scraping #####
+
+
+def init_splinter(headlessTF=True):
+    """ opens Splinter for scraping """
+    # path to Splinter's chromedriver
+    executable_path = {'executable_path': 'resources/chromedriver.exe'}
+    # start Splinter
+    return Browser('chrome', **executable_path, headless=headlessTF)
+
+
+def init_beautifulsoup(htmlpage_variable, save_as=""):
+
+    # test for save_as. Skip this step if blank, but save file if the argument has been given.
+    if save_as != "":
+        save_html(htmlpage_variable, save_as)
+
+    # return Beautiful Soup object of selected webpage
+    return BeautifulSoup(htmlpage_variable, "lxml")
+
+
+def save_html(htmlpage_variable, page_to_save):
+    """ Save selected variable to an html file """
+
+    # # check if "data" directory exists within current file path. If not: create it.
+    # # "data" is where we save off the html files returned from Splinter
+    # if not os.path.exists('data'):
+    #     os.makedirs('data')
+
+    try:
+        print(page_to_save)
+        with open(page_to_save, 'w', encoding="UTF-8") as f:
+            f.write(htmlpage_variable)
+    except:
+        print("scrape failed")
+
+
+def scrape_web_page(page_url, save_as=""):
+    """ Function to scrape individual webpages, using Splinter and Beautiful Soup.
+
+        This function should only be used if Splinter doesn't need to interact with a web page.
+
+        Keyword Arguments:
+        page_url -- Page to scrape
+        save_as -- Local ile path and name to save the parsed HTML file to.
+
+        Returns:
+        BeautifulSoup Result Set
+     """
+
+    # open Splinter and visit web page
+    browser = init_splinter(headlessTF=False)
+    browser.visit(page_url)
+
+    # pause for 3 seconds to let the page load
+    time.sleep(3)
+
+    # get html from web page and save
+    soup = init_beautifulsoup(browser.html, save_as)
+
+    # quit Splinter
+    browser.quit
+
+    # return Beautiful Soup object of selected webpage
+    return soup
---
+++ noted.md
@@ -0,0 +1,13 @@
+Get All links 
+if links have a UL element, only get the html from <li> to <ul>. How? 
+
+an li can have four main elements: 
+1. "see as " text / placeholder text
+2. the <a> link text</a>
+3. author text 
+4. a <ul> with all of the same 
+
+for each link:
+    is the first element text or an href:
+        if the first element is text, is it placeholder text or see as text:
+            
