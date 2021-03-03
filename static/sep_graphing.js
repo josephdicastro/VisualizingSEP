de762e9eff8db08a7a8d47dcbe2c4397c70cc66f
josephdicastro <joe.dicastro@gmail.com>
2021-03-02 19:31:38 -0500

    testing processurl

--- static/sep_graphing.js
+++ static/sep_graphing.js
@@ -939,7 +939,7 @@ function processURL() {
 }
 
 function processArticle(urlHash) {
-    console.log(urlhash)
+    console.log(urlHash)
     let hashPosition = urlHash.indexOf('#') + 1
     let articleID = urlHash.substring(hashPosition)
     let articleObj = allArticles.filter(article => article.id === articleID)
